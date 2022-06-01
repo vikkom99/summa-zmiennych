dfca007e7b298a4cf8379839eb6c0f68b95dc324
komarov <wikkomarik@gmail.com>
2022-06-01 14:18:56 +0100

    summa

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let firstNumber = 20;
+let secondNumber = 55;
+
+function plus(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+}
+console.log(plus(20, 55));
+
+
