7c7c5e0d321876d307fbb2168883392e36f29e0a
vikkom99 <wikkomarik@gmail.com>
2022-06-12 22:32:21 +0100

    poprawione

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 let firstNumber = 20;
 let secondNumber = 55;
 
-function plus(firstNumber, secondNumber) {
-    let result = firstNumber + secondNumber;
+function plus(a, b) {
+    let result = a + b;
     return result;
 }
-console.log(plus(20, 55));
+console.log(plus(firstNumber, secondNumber));
 
 
